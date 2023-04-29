31332e88feca19e66bdbec73374147bb4d13eddb
Rachel Hamby <tnthreat33@gmail.com>
2023-04-29 12:05:48 -0400

    team css

--- src/Teams.js
+++ src/Teams.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import './Team.css';
 
 function Teams({teams}){
 
   return (
-    teams.length > 0 &&
-    teams.map((team) => (
-      <div key={team.id} className="card">
-        <h4>
-          {team.name} 
-        </h4>
-        <p>Location: {team.city}, {team.state}</p>
-        
-      </div>
-    ))
+    <div className="teams">
+      {teams.length > 0 &&
+        teams.map((team) => (
+          <div key={team.id} className="card">
+            <h4>
+              {team.name} 
+            </h4>
+            <p>Location: {team.city}, {team.state}</p>
+          </div>
+        ))
+      }
+    </div>
   );
 }
 
